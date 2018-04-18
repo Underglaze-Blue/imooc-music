b0fb376188852154b3484feaa335840527e58bd0
Underglaze-Blue <lepapillonangel@gmail.com>
2018-04-18 11:43:05 +0800

    完善searchHistory列表元素删除过渡

--- src/base/searchList/searchList.vue
+++ src/base/searchList/searchList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="search-list" v-show="searches.length">
-    <ul>
-      <li @click="selectItem(item)" class="search-item" v-for="(item, index) in searches" :key="index">
-        <span class="text">{{item}}</span>
-        <span class="icon" @click.stop="deleteOnde(item)">
-          <i class="icon-delete"></i>
-        </span>
-      </li>
-    </ul>
+      <transition-group name="list" tag="ul">
+        <li @click="selectItem(item)" class="search-item" v-for="(item, index) in searches" :key="index">
+          <span class="text">{{item}}</span>
+          <span class="icon" @click.stop="deleteOnde(item)">
+            <i class="icon-delete"></i>
+          </span>
+        </li>
+      </transition-group>
   </div>
 </template>
 
@@ -37,15 +37,16 @@ export default {
 
 <style rel="stylesheet/stylus" lang="stylus">
 @import '~common/stylus/variable'
+@import '~common/stylus/mixin'
 .search-list
   .search-item
     display flex
     align-items center
     height 40px
     overflow hidden
-    &.list-enter-active,&.list-leave-active
-      transition all .1s
-    &.list-enter,&.list-leave-to
+    &.list-enter-active, &.list-leave-active
+      transition all 0.1s
+    &.list-enter, &.list-leave-to
       height 0
     .text
       flex 1
