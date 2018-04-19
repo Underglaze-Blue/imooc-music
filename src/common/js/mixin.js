e165bb8d3293f45e757966ebce8a7885fe2b9321
Underglaze-Blue <lepapillonangel@gmail.com>
2018-04-19 15:13:56 +0800

    抽象 favorite 点击 mixin

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -35,7 +35,8 @@ export const playerMixin = {
       'sequenceList',
       'currentSong',
       'mode',
-      'playlist'
+      'playlist',
+      'favoriteList'
     ])
   },
   methods: {
@@ -57,12 +58,28 @@ export const playerMixin = {
       });
       this.setCurrentIndex(index);
     },
+    getFavoriteIcon (song) {
+      return this.isFavorite(song) ? 'icon-favorite' : 'icon-not-favorite';
+    },
+    toggleFavorite (song) {
+      this.isFavorite(song) ? this.deleteFavoriteList(song) : this.saveFavoriteList(song);
+    },
+    isFavorite (song) {
+      const index = this.favoriteList.findIndex((item) => {
+        return item.id === song.id;
+      });
+      return index > -1;
+    },
     ...mapMutations({
       setPlayingState: 'SET_PLAYING_STATE',
       setCurrentIndex: 'SET_CURRENT_INDEX',
       setPlayMode: 'SET_PLAY_MODE',
       setPlaylist: 'SET_PLAYLIST'
-    })
+    }),
+    ...mapActions([
+      'saveFavoriteList',
+      'deleteFavoriteList'
+    ])
   }
 };
 
